da3d396f04ccb3fa8644530e5d22c47ad548a665
Ivanchik <iradolgaya88@icloud.com>
2025-03-04 00:42:18 +0400

    first commit

---
+++ app/js/main.js
@@ -0,0 +1,147 @@
+$ (function () {
+
+  $(".menu__btn").on("click", function() {
+    $(".menu__list").slideToggle('menu__list--active');
+  });
+
+  $(".shop__filter-btn").on("click", function () {
+    $(".shop__filters").slideToggle();
+  });
+
+
+  $(".blog-page__slider").slick({
+    infinite: false,
+    prevArrow:
+      '<button type="button" class="slick-prev"><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="9px" height="px" viewBox="0 0 9 14" version="1.1"><g id="surface1"><path d="M 1.164062 6.382812 C 0.8125 6.722656 0.8125 7.277344 1.164062 7.621094 L 5.664062 11.996094 C 6.015625 12.335938 6.585938 12.335938 6.9375 11.996094 C 7.289062 11.652344 7.289062 11.097656 6.9375 10.757812 L 3.074219 7 L 6.9375 3.242188 C 7.289062 2.902344 7.289062 2.347656 6.9375 2.003906 C 6.585938 1.664062 6.011719 1.664062 5.660156 2.003906 L 1.160156 6.378906 Z M 1.164062 6.382812 " /></g></svg></button>',
+    nextArrow:
+      '<button type="button" class="slick-next"><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="9px" height="px" viewBox="0 0 9 14" version="1.1"><g id="surface1"><path  d="M 7.835938 6.382812 C 8.1875 6.722656 8.1875 7.277344 7.835938 7.621094 L 3.335938 11.996094 C 2.984375 12.335938 2.414062 12.335938 2.0625 11.996094 C 1.710938 11.652344 1.710938 11.097656 2.0625 10.757812 L 5.925781 7 L 2.0625 3.242188 C 1.710938 2.902344 1.710938 2.347656 2.0625 2.003906 C 2.414062 1.664062 2.988281 1.664062 3.339844 2.003906 L 7.839844 6.378906 Z M 7.835938 6.382812 "/></g></svg></button>',
+  });
+
+  $(".product-tabs__top-item").on("click", function (e) {
+    e.preventDefault();
+    $(".product-tabs__top-item").removeClass("product-tabs__top-item--active");
+    $(this).addClass("product-tabs__top-item--active");
+    $(".product-tabs__content-item").removeClass(
+      "product-tabs__content-item--active"
+    );
+    $($(this).attr("href")).addClass("product-tabs__content-item--active");
+  });
+  $(".shop-content__filter-btn").on("click", function () {
+    $(".shop-content__filter-btn").removeClass(
+      "shop-content__filter-btn--active"
+    );
+    $(this).addClass("shop-content__filter-btn--active");
+  });
+
+
+  $(".product-slide__thumb").slick({
+    asNavFor: ".product-slide__big",
+    focusOnSelect: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    vertical: true,
+    draggable: false,
+  });
+  $(".product-slide__big").slick({
+    asNavFor: ".product-slide__thumb",
+    arrows: false,
+    fade: true,
+    responsive: [
+      {
+        breackpoint: 1051,
+        settings: {
+          draggable: true,
+        },
+      },
+    ],
+  });
+  
+  $('.shop-content__filter-btn').on('click', function () {
+    $('.shop-content__filter-btn').removeClass('shop-content__filter-btn--active');
+    $(this).addClass('shop-content__filter-btn--active');
+  });
+
+  $(".catalog__filters-btn").on("click", function () {
+    $(".catalog__filters").toggleClass("catalog__filters--active");
+  });
+
+  $(".catalog__filters-close").on("click", function () {
+    $(".catalog__filters").toggleClass("catalog__filters--active");
+  });
+
+  $(".select-style, .product-one__item-num").styler();
+  
+  $('.filter-price__input').ionRangeSlider({
+    type: "double",
+    prefix: "$",
+    onStart: function (data){
+      $(".filter-price__from").text(data.from);
+      $(".filter-price__to").text(data.to);
+    },
+    onChange: function (data) {
+      $('.filter-price__from').text(data.from);
+      $('.filter-price__to').text(data.to);
+    },
+  });
+
+  $('.top-slider__inner').slick({
+    dots: true,
+    arrows: false,
+    fade: true,
+    autoplay: true,
+    autoplaySpeed: 2000
+  });
+
+  $(".star").rateYo({
+    starWidth: "17px",
+    normalFill: "#ccccce",
+    ratedFill: "#ffc35b",
+    readOnly: true,
+  });
+
+  function getTimeRemaining(endtime) {
+    const total = Date.parse(endtime) - Date.parse(new Date());
+    const seconds = Math.floor((total / 1000) % 60);
+    const minutes = Math.floor((total / 1000 / 60) % 60);
+    const hours = Math.floor((total / (1000 * 60 * 60)) % 24);
+    const days = Math.floor(total / (1000 * 60 * 60 * 24));
+
+    return {
+      total,
+      days,
+      hours,
+      minutes,
+      seconds,
+    };
+  }
+
+  function initializeClock(id, endtime) {
+    const clock = document.querySelector(".promo__clock");
+    const daysSpan = clock.querySelector(".promo__days");
+    const hoursSpan = clock.querySelector(".promo__hours");
+    const minutesSpan = clock.querySelector(".promo__minutes");
+    const secondsSpan = clock.querySelector(".promo__seconds");
+
+    function updateClock() {
+      const t = getTimeRemaining(endtime);
+
+      daysSpan.innerHTML = t.days;
+      hoursSpan.innerHTML = ("0" + t.hours).slice(-2);
+      minutesSpan.innerHTML = ("0" + t.minutes).slice(-2);
+      secondsSpan.innerHTML = ("0" + t.seconds).slice(-2);
+
+      if (t.total <= 0) {
+        clearInterval(timeinterval);
+      }
+    }
+
+    updateClock();
+    const timeinterval = setInterval(updateClock, 1000);
+  }
+
+  const deadline = $('.promo__clock').attr('data-time');
+  initializeClock("promo__clockpromo__", deadline);
+
+  
+
+});
